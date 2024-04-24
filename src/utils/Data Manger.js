2a572c4ea1298a62f7514146d5c4f71182123d29
AceReX <ace.rexnoir@gmail.com>
2024-04-24 21:35:37 +0530

    Implement Data Manger

---
+++ src/utils/Data Manger.js
@@ -0,0 +1,32 @@
+export class Data {
+  constructor(title) {
+    this.title = title;
+    this.id = title.slice(0, 2);
+    this.watermark = "json";
+  }
+  setDate(date) {
+    this.date = date;
+    return this;
+  }
+  setNote(note) {
+    this.note = note;
+    return this;
+  }
+  build() {
+    return JSON.stringify(this);
+  }
+}
+export class Storage {
+  static getSotrageLength() {
+    return localStorage.length;
+  }
+  static getDataOf(key) {
+    return JSON.parse(localStorage.getItem(key));
+  }
+  static removeData(key) {
+    localStorage.removeItem(key);
+  }
+  static clearStorageData() {
+    localStorage.clear();
+  }
+}
