b0517d394bb76034bfd4879c5559956be2f3cc78
AceReX <ace.rexnoir@gmail.com>
2024-04-24 21:51:54 +0530

    Grid the #app with two rows

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import { newHomeTemplate } from "./src/layout/home";
 import "./style.css";
 import "./src/layout/navigation.js";
+import { Data } from "./src/utils/Data Manger.js";
 
 let app = document.querySelector("#app");
 const homepage = newHomeTemplate();
