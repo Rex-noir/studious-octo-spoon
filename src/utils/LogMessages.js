a69ff364529110b6e561dafacb1e31ba52b2954e
AceReX <ace.rexnoir@gmail.com>
2024-04-25 22:58:51 +0530

    Add new LOG class for easier loggin

---
+++ src/utils/LogMessages.js
@@ -0,0 +1,33 @@
+export class Log {
+  constructor(logElement) {
+    this.logElement = logElement;
+    this.emptyView =
+      "You haven't view any data! You can add or view your previous datas from the list.";
+    this.notSave = "Not saved";
+    this.invalidInput = "Title & DeadLine must not be empty!";
+    this.titleLimit = "Title character limit reached!";
+    this.emptyViewShort = "No view";
+  }
+  getMessages() {
+    return this;
+  }
+  setMessage(message) {
+    this.logElement.textContent = message;
+    return this;
+  }
+  type(type) {
+    switch (type) {
+      case "error":
+        this.logElement.style.color = "red";
+        break;
+      case "warning":
+        this.logElement.style.color = "yellow";
+        break;
+      case "success":
+        this.logElement.style.color = "green";
+        break;
+      default:
+        this.logElement.style.color = "";
+    }
+  }
+}
