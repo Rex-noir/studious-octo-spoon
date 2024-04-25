251974fe18c6f614b4b57850baa4a167c011046e
AceReX <ace.rexnoir@gmail.com>
2024-04-26 01:32:27 +0530

    Fix open varibale not updating correctly

--- src/layout/navigation.js
+++ src/layout/navigation.js
@@ -6,28 +6,29 @@ const nav_menu = document.querySelector(".nav-menu");
 const nav_button = document.querySelector(".nav-menu-icon");
 const nav_logo = document.querySelector(".nav-logo");
 
-let open = false;
+let open = true;
 nav_menu.addEventListener("click", () => {
-  open = !open;
   if (open) {
-    nav_button.setAttribute("src", closeIcon);
-    nav_logo.style.display = "none";
     openNav();
   } else {
-    nav_button.setAttribute("src", openIcon);
-    nav_logo.style.display = "flex";
     closeNav();
   }
 });
 
 export function openNav() {
+  open = !open;
   document.querySelector(".side-nav-container").style.width = "240px";
   document.querySelector("#app").style.marginLeft = "250px";
+  nav_button.setAttribute("src", closeIcon);
+  nav_logo.style.display = "none";
 }
 
 export function closeNav() {
+  open = !open;
   document.querySelector(".side-nav-container").style.width = "0";
   document.querySelector("#app").style.marginLeft = "0";
+  nav_button.setAttribute("src", openIcon);
+  nav_logo.style.display = "flex";
 }
 //new button
 const nav_new = document.querySelector(".nav-new");
