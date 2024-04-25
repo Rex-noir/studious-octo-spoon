43171c1460b4c41b1f399de16bdbabc4ea3c553e
AceReX <ace.rexnoir@gmail.com>
2024-04-26 01:04:11 +0530

    Add deleting items img button

--- src/layout/home.js
+++ src/layout/home.js
@@ -1,8 +1,11 @@
 import { createElement } from "../utils/DOM-manipulator";
 import { View, closedState } from "../utils/ViewContent";
 import "/src/styles/layout.css";
+import deleteBUTTONIcon from "/src/img/delete-button.svg";
 
 const container = createElement("div", ["home", "wrapper"]);
+const taskLists = document.querySelector(".task-lists");
+
 let open = false;
 export function newHomeTemplate() {
   if (!open) {
@@ -12,4 +15,30 @@ export function newHomeTemplate() {
 }
 export function getView() {
   new View({ option: "add", container: container });
+}
+
+//task list handler
+fillTheNav();
+export function fillTheNav() {
+  taskLists.innerHTML = "";
+  const keys = Object.keys(localStorage);
+
+  for (const key of keys) {
+    const listItemContainer = createElement("div", ["list-item-container"]);
+    const li = createElement("li", ["list-item"]);
+
+    li.textContent = key;
+    li.addEventListener("click", (e) => {
+      new View({ option: "view", container: container, key: key });
+    });
+
+    const DeleteBTN = createElement("img", ["item-delete-button"]);
+    DeleteBTN.textContent = "DELETE";
+    DeleteBTN.src = deleteBUTTONIcon;
+    DeleteBTN.style.width = "24px";
+
+    listItemContainer.appendChild(li);
+    listItemContainer.appendChild(DeleteBTN);
+    taskLists.appendChild(listItemContainer);
+  }
 }
